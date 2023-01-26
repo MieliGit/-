d95b89bba7c149aa3ee8119792e8d4f605841222
Zakharov Daniil <zdan14@mail.ru>
2023-01-26 22:20:47 +0300

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+//  а на выходе показывает все целые числа в промежутке от 
+//  -N до N. 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2"
+
+Console.Clear();
+Console.WriteLine("Введите целое число");
+int Number = int.Parse(Console.ReadLine());
+int OneNumber;
+int TwoNumber;
+
+if(Number < 0) 
+{
+    OneNumber = Number;
+    TwoNumber = Number*(-1);
+}
+else
+{
+    OneNumber = Number*(-1);
+    TwoNumber = Number;
+}
+while(OneNumber < TwoNumber)
+{
+    Console.Write(OneNumber +", ");
+    OneNumber++;
+}
+Console.WriteLine(TwoNumber);
