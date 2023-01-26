c1fa7da8a965d752a526df58a91e1c23d11444da
Zakharov Daniil <zdan14@mail.ru>
2023-01-26 22:35:51 +0300

    Add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6 782 -> 2 918 -> 8
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int Number = int.Parse(Console.ReadLine());
+
+if(Number > 999)
+{
+    Console.WriteLine ("Введено неверное число");
+}
+else 
+{
+    Number= Number%100%10;
+    Console.WriteLine(Number);
+}
+
+   
