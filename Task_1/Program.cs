5a726ebaf5752d1440bd7b2fdcde67c4feb9fdba
Zakharov Daniil <zdan14@mail.ru>
2023-01-26 21:31:34 +0300

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Проверка квадрата числа 
+
+Console.Clear ();
+Console.Write ("Введите число один: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int sqr1 = number1*number1;
+int sqr2 = number2*number2;
+
+if (number1==sqr2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else if (number2==sqr1)
+    {
+    Console.WriteLine("Второе число является квадратом первого");
+    }
