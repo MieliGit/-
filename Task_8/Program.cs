cd8fbb51a3071e08a4e2bc18e9128af0156cdc25
Zakharov Daniil <zdan14@mail.ru>
2023-01-30 20:58:26 +0300

    Add Task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Clear();
+int n = new Random().Next(7, 1000);
+int a = 7;
+int b = 23;
+if(n%7 == 0) 
+{
+    if(n%23 == 0) 
+    {
+        Console.WriteLine($"{n} -> Yes");
+    }
+    else 
+    {
+        Console.WriteLine($"{n} -> No");
+    }
+}
+else
+{
+    Console.WriteLine($"{n} -> No");
+}
+
