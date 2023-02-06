94ed4cb47f0d08adfe45274829d85ed2509bc404
Zakharov Daniil <zdan14@mail.ru>
2023-02-06 20:59:53 +0300

    Add Task_17

---
+++ Task_17/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая принимает
+// на вход число N и выдаёт произведение чисел
+// от 1 до N. 4 -> 24 5 -> 120
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение цифк = {MultiplyN(n)}");
+
+int MultiplyN(int B)
+{
+    int multi = 1;
+    while(B > 0)
+    {
+        multi = multi*B;
+        B--;
+    }
+    return multi;
+}
