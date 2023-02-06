e1d36f81299426351ce3cb85e8579a3ef6e1fc5e
Zakharov Daniil <zdan14@mail.ru>
2023-02-06 20:35:05 +0300

    Add Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифк = {SumN(n)}");
+
+int SumN(int B)
+{
+    int sum = 0;
+    while(B > 0)
+    {
+        sum += B;
+        B--;
+    }
+    return sum;
+}
