6a3fff6fcfdbd2f3f33af1ff38c0ecc7be595cc0
Zakharov Daniil <zdan14@mail.ru>
2023-02-06 20:52:17 +0300

    Add Task_16

---
+++ Task_16/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26: Напишите программу, которая принимает на 
+// вход число и выдаёт количество цифр в числе. 
+// 456 -> 3 78 -> 2 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Колличество цифр = {SizeN(n)}");
+
+int SizeN(int number)
+{
+    int size = 1;
+    while(number > 10)
+    {
+        number = number/10;
+        size++;
+    }
+    return size;
+}
