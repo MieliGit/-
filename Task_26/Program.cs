9da7b0d514eda83ca7d85f3c07622dfb74e9f500
Zakharov Daniil <zdan14@mail.ru>
2023-02-16 02:48:32 +0300

    Add Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+int n1 = Input("Введите первое число");
+Console.WriteLine(Binary(n1));
+
+
+string Binary(int someNumber)
+{
+    string str = "";
+    do
+    {
+        str = Convert.ToString(someNumber%2) + str;
+        someNumber = someNumber/2;
+    }  while(someNumber != 0);
+    return str;
+}
+
+int Input(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
