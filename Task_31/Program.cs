9d415787bb74334e45a3e5cc5fe7180e961cd997
Zakharov Daniil <zdan14@mail.ru>
2023-02-19 00:01:33 +0300

    Add Task_31

---
+++ Task_31/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы,
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+Console.Clear();
+
+int rows = Input("Введите число строк");
+int colunns = Input("Введите число столбцов");
+
+
+int[,] array = getArray(rows, colunns, 0 , 10);
+int[,] multi = array;
+PrintArray(array);
+Console.WriteLine();
+Multi(multi);
+PrintArray(multi);
+
+int[,] getArray( int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+void Multi(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            if(i%2==0 && j%2==0) inArray[i,j] = inArray[i,j] * inArray[i,j];
+        }
+    }
+}
