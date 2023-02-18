1f081b0bfe0ac65fd90d07c5e2d7f9e4311de732
Zakharov Daniil <zdan14@mail.ru>
2023-02-19 00:08:42 +0300

    Add Task_32

---
+++ Task_32/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Clear();
+
+int rows = Input("Введите число строк");
+int colunns = Input("Введите число столбцов");
+
+int[,] array = getArray(rows, colunns, 0 , 10);
+int[,] sum = array;
+PrintArray(array);
+Console.WriteLine();
+Sum(sum);
+
+int[,] getArray( int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+void Sum(int[,] inArray)
+{
+    int n = 0;
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            if(i==j) n= n+ inArray[i,j];
+        }
+    }
+    Console.WriteLine($"Сумма элементов главной диагонали: {n}");
+}
