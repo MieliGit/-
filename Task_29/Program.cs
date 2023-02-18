9feeb14ac466c780c8b373941bd28dcd08dd138a
Zakharov Daniil <zdan14@mail.ru>
2023-02-18 23:33:58 +0300

    Add Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Clear();
+
+int rows = Input("Введите число строк");
+int colunns = Input("Введите число столбцов");
+
+
+int[,] array = getArray(rows, colunns, 0 , 10);
+PrintArray(array);
+
+int[,] getArray( int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
