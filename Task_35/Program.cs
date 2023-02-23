6ba65c04cfdc68ef90ebbee439e41bd6f5e55ae1
Zakharov Daniil <zdan14@mail.ru>
2023-02-23 22:16:40 +0300

    Add Task_35

---
+++ Task_35/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, 
+// сколько раз встречается элемент входных данных.
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+// 0 встречается 2 раза
+// 1 встречается 1 раз
+// 2 встречается 1 раз
+// 8 встречается 1 раз
+// 9 встречается 3 раза
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+Console.Clear();
+
+int rows = Input("Введите число строк");
+int colunns = Input("Введите число столбцов");
+int min = Input("Введите минимальное число в массиве");
+int max = Input("Введите максимальное чило в массиве");
+
+int[,] array = getArray(rows, colunns, min , max);
+int[,] sum = array;
+
+PrintArray(array);
+Counter(array, min);
+Console.WriteLine();
+
+int[,] getArray( int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+void Counter(int[,] inArray, int num)
+{   
+    while(num <= max)
+    {
+    int count = 0;
+   
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            if(inArray[i,j] == num)
+            {
+            count++;
+            } 
+        }
+    }
+    if (count != 0)
+    {
+    Console.WriteLine($"Число {num} встречается -> {count} раз");
+    }
+    num++;
+    }
+}
