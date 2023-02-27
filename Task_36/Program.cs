f816b8bd109d40725ef05c5e14bb7c727d9f8169
Zakharov Daniil <zdan14@mail.ru>
2023-02-27 15:31:28 +0300

    Add TAsk_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+ 
+int n = Input("Введите целое число");
+int n1 = 1;
+NumberListRec(n, n1);
+
+void NumberListRec(int b, int a)
+{
+   
+    Console.Write($"{a} ");
+    if(a == b) return;
+    else NumberListRec(b, a + 1);
+}
+
+
+int Input(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
